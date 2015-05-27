cd128134fbcfb99a9bc18829bc55f5c708ad4584
ade yahya <adeyahyaprasetyo@gmail.com>
2015-05-28 01:58:46 +0700

    cleaning code

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -17,31 +17,6 @@ class CommentController extends Controller {
 		$comments = Comment::where('story_id',$storyid)->with('user')->get();
 		return $comments;
 	}
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
 	public function store()
 	{
 		
@@ -70,49 +45,4 @@ class CommentController extends Controller {
 		}
 		return Comment::where('id',$comment->id)->with('user')->first();
 	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
 }
